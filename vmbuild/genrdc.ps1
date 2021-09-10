c0b5b1709243a462e1ab814fea2b1fc149111518
timhe <timhe@microsoft.com>
2021-09-10 17:50:30 -0400

    refactor and fix rdc generator

--- vmbuild/genrdc.ps1
+++ vmbuild/genrdc.ps1
@@ -1,74 +1,105 @@
 . $PSScriptRoot\Common.ps1
 $userName = "admin"
-Write-Warning "UserName is $userName"
-Write-Warning "Password is $($Common.LocalAdmin.Password)"
+#$result = Test-Configuration -FilePath "E:\repos\memlabs\vmbuild\config\samples\Standalone.json"
+#$result = Test-Configuration -FilePath "E:\repos\memlabs\vmbuild\config\samples\Hierarchy.json"
+$result = Test-Configuration -FilePath "E:\repos\memlabs\vmbuild\config\samples\AddToExisting.json"
+if ($result.Valid) {
+$deployConfig = $result.DeployConfig
+}
 function New-RDCManFile($rdcmanfile){
-    $templatefile = Join-Path $PSScriptRoot "template.rdg"
-    
-    if ((test-path "$rdcmanfile")) {
-        $templatefile = $rdcmanfile
-    }
 
-    [xml]$template = Get-Content -Path $templatefile
-    
+    $template = Get-RDCManTemplate ($rdcmanfile)
+
+    #This is the bulk of the data.
     $file = $template.RDCMan.file
-    
     $group = $file.group
+    # <RDCMan>
+    #   <file>         
+    #     <group>
+    #        <logonCredentials>
+    #        <server>
+    #        <server>
+    #     <group>
+    #     ...
+
+    $domain = $deployConfig.vmOptions.domainName
+    $findGroup = Get-RDCManGroupToModify
+    foreach ($vm in $deployConfig.virtualMachines)
+    {
+        Add-RDCManServerToGroup ($vm.vmName)
+    }
     
-    $domain = "fabrikam.com"
-    $servername1 = "PS-DC1"
-    $servername2 = "PS-PRI1"
+    #If the original file was a template, remove the templated group.
+    if ($group.properties.Name -eq "VMASTEMPLATE"){
+        [void]$file.RemoveChild($group)
+    }
+    [void]$file.AppendChild($findgroup)
     
+    #Save to desired filename
+    Write-Host "Saving resultant XML to $rdcmanfile"
+    $template.save($rdcmanfile) | Out-Null
+    
+}
+
+function Add-RDCManServerToGroup([string]$serverName){
+    $findserver = $findgroup.server | Where-Object { $_.properties.name -eq $serverName} | Select-Object -First 1
+    Write-Host "Adding $serverName to RDG Group ... " -NoNewline
+    if ($null -eq $findserver){
+      Write-Host "added"
+      $server = $group.SelectNodes('//server') | Select-Object -First 1
+      $newserver = $server.clone()
+      $newserver.properties.name = $serverName
+      $findgroup.AppendChild($newserver)   | Out-Null   
+    }else{
+        Write-Host "already exists in group. Skipping"
+    }
+}
+
+#This gets the <Group> section from the template. Either makes a new one, or returns an existing one.
+#If a new one is created, the <server> nodes will not exist.
+function Get-RDCManGroupToModify(){
+    Write-Host "Looking for group entry named $domain in current xml... " -NoNewline
     $findGroup = $group | Where-Object { $_.properties.name -eq $domain} | Select-Object -First 1
     
     if ($null -eq $findGroup){
+        Write-Host "Not found.  Creating new group"
         $findGroup = $group.Clone()    
         $findGroup.properties.name = $domain
         $findGroup.logonCredentials.userName = $userName
         $findGroup.logonCredentials.password = Get-RDCManPassword
         $findGroup.logonCredentials.domain = $domain
         $ChildNodes = $findGroup.SelectNodes('//server')
-        Write-Host $ChildNodes
         foreach($Child in $ChildNodes){
-            Write-Host Removing $Child
             [void]$Child.ParentNode.RemoveChild($Child)
         }    
+    }else{
+        Write-Host "Found!"
     }
-    $server = $group.SelectNodes('//server') | Select-Object -First 1
-    $findserver = $group.server | Where-Object { $_.properties.name -eq $servername1} | Select-Object -First 1
-    if ($null -eq $findserver){
-    $newserver = $server.clone()
-    $newserver.properties.name = $servername1
-    $findGroup.AppendChild($newserver)
+    return $findGroup
+}
+
+#Gets the blank template, or returns the existing rdg xml if available.
+function Get-RDCManTemplate($rdcmanfile){
+    $templatefile = Join-Path $PSScriptRoot "template.rdg"
+    
+    if ((test-path "$rdcmanfile")) {
+        $templatefile = $rdcmanfile
     }
-    
-    $findserver = $group.server | Where-Object { $_.properties.name -eq $servername2} | Select-Object -First 1
-    if ($null -eq $findserver){
-    $newserver = $server.clone()
-    $newserver.properties.name = $servername2
-    $findGroup.AppendChild($newserver)
-    }
-    if ($group.properties.Name -eq "VMASTEMPLATE"){
-        [void]$file.RemoveChild($group)
-    }
-    [void]$file.AppendChild($findgroup)
-    
-    
-    $template.save($rdcmanfile)
-    
+    Write-Host "Loading existing XML from $templatefile"
+    [xml]$template = Get-Content -Path $templatefile
+    return $template
 }
 
 function Get-RDCManPassword(){
-    $rdcmanpath = "C:\tools\"
+    $rdcmanpath = "C:\ProgramData\chocolatey\bin"
     if (-not(test-path "$($env:temp)\rdcman.dll")) {
-        copy-item "$($rdcmanpath)\rdcman.exe" "$($env:temp)\rdcman.dll"
-        unblock-file "$($env:temp)\rdcman.dll"
+        copy-item "$($rdcmanpath)\rdcman.exe" "$($env:temp)\rdcman.dll" | Out-Null
+        unblock-file "$($env:temp)\rdcman.dll" | Out-Null
      }
     
-    Import-Module "$($env:temp)\rdcman.dll"
+    Import-Module "$($env:temp)\rdcman.dll" |Out-Null
     $EncryptionSettings = New-Object -TypeName RdcMan.EncryptionSettings
     return [RdcMan.Encryption]::EncryptString($Common.LocalAdmin.Password , $EncryptionSettings)
 }
-
 
 New-RDCManFile "C:\Tools\new4.rdg"
