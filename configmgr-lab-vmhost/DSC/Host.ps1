65607ed8c7fc7841b6885e9855004052371524a4
vinaypamnani-msft <vinpa@microsoft.com>
2021-09-16 18:43:12 -0400

    Add DSC, move script code to configureHost.ps1

--- configmgr-lab-vmhost/DSC/Host.ps1
+++ configmgr-lab-vmhost/DSC/Host.ps1
@@ -2,13 +2,9 @@ Configuration Host {
 
     Import-DscResource -ModuleName 'PSDesiredStateConfiguration', 'xHyper-V', 'xNetworking', 'xDscDiagnostics'
 
-    $externalSwitchName = "External"
-
-    $phsyicalNic = Get-NetAdapter | Where-Object {$_.InterfaceDescription -like "Microsoft Hyper-V Network Adapter*" }
+    $phsyicalNic = Get-NetAdapter | Where-Object { $_.InterfaceDescription -like "Microsoft Hyper-V Network Adapter*" }
     $phsyicalInterface = $phsyicalNic.Name
-
-    $repoName = "memlabs"
-    $repoUrl = "https://github.com/vinaypamnani-msft/$repoName"
+    $externalSwitchName = "External"
 
     Node LOCALHOST {
 
@@ -16,8 +12,6 @@ Configuration Host {
             ConfigurationMode  = 'ApplyOnly'
             RebootNodeIfNeeded = $true
         }
-
-        # Windows Features
 
         WindowsFeature Hyper-V {
             Ensure               = 'Present'
@@ -37,116 +31,12 @@ Configuration Host {
             IncludeAllSubFeature = $true
         }
 
-        # Hyper-V Host Network Settings
-
-        xVMSwitch ExternalSwitch
-        {
+        xVMSwitch ExternalSwitch {
             DependsOn      = '[WindowsFeature]Hyper-V-PowerShell'
             Ensure         = 'Present'
             Name           = $externalSwitchName
             Type           = 'External'
             NetAdapterName = $phsyicalInterface
-        }
-
-        # Storage Pool
-
-        Script MoveCDDrive
-        {
-            DependsOn = '[xVMSwitch]ExternalSwitch'
-            SetScript = {
-                # Start logging the actions
-                Start-Transcript -Path $env:windir\temp\CDMovelog.txt -Append -Force
-
-                # Move CD-ROM drive to Z:
-                "Moving CD-ROM drive to Z:.."
-                Get-WmiObject -Class Win32_volume -Filter 'DriveType=5' | Select-Object -First 1 | Set-WmiInstance -Arguments @{DriveLetter='Z:'}
-
-                Stop-Transcript
-            }
-
-            TestScript = {
-                $x = Get-WmiObject -Class Win32_volume -Filter 'DriveType=5 AND DriveLetter="Z:"'
-                if ($x) {return $true } else { return $false }
-            }
-
-            GetScript = {
-                # Do nothing
-            }
-        }
-
-        Script StoragePool {
-            DependsOn = "[Script]MoveCDDrive"
-            SetScript = {
-                New-StoragePool -FriendlyName StoragePool1 -StorageSubSystemFriendlyName '*storage*' -PhysicalDisks (Get-PhysicalDisk -CanPool $True)
-            }
-            TestScript = {
-                (Get-StoragePool -ErrorAction SilentlyContinue -FriendlyName StoragePool1).OperationalStatus -eq 'OK'
-            }
-            GetScript = {
-                @{Ensure = if ((Get-StoragePool -FriendlyName StoragePool1).OperationalStatus -eq 'OK') {'Present'} Else {'Absent'}}
-            }
-        }
-
-        Script VirtualDisk {
-            DependsOn = "[Script]StoragePool"
-            SetScript = {
-              $disks = Get-StoragePool -FriendlyName StoragePool1 -IsPrimordial $False | Get-PhysicalDisk
-              $diskNum = $disks.Count
-              New-VirtualDisk -StoragePoolFriendlyName StoragePool1 -FriendlyName VirtualDisk1 -ResiliencySettingName simple -NumberOfColumns $diskNum -UseMaximumSize
-            }
-            TestScript = {
-              (get-virtualdisk -ErrorAction SilentlyContinue -friendlyName VirtualDisk1).OperationalStatus -eq 'OK'
-            }
-            GetScript = {
-              @{Ensure = if ((Get-VirtualDisk -FriendlyName VirtualDisk1).OperationalStatus -eq 'OK') {'Present'} Else {'Absent'}}
-            }
-        }
-
-        Script FormatDisk {
-            DependsOn = "[Script]VirtualDisk"
-            SetScript = {
-                Get-VirtualDisk -FriendlyName VirtualDisk1 | Get-Disk | Initialize-Disk -Passthru | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -NewFileSystemLabel VirtualDisk1 -AllocationUnitSize 64KB -FileSystem NTFS
-            }
-            TestScript = {
-                (get-volume -ErrorAction SilentlyContinue -filesystemlabel VirtualDisk1).filesystem -EQ 'NTFS'
-            }
-            GetScript = {
-                @{Ensure = if ((get-volume -filesystemlabel VirtualDisk1).filesystem -EQ 'NTFS') {'Present'} Else {'Absent'}}
-            }
-        }
-
-        # Clone repo
-
-        Script DownloadFiles {
-            DependsOn = "[Script]FormatDisk"
-            SetScript = {
-                Start-Transcript -Path $env:windir\temp\RepoClone.txt -Append -Force
-
-                "Downloading and installing chocolatey"
-                Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-
-                "Reloading PATH"
-                $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
-
-                "Installing git via choco"
-                & choco install git -y
-
-                "Installing sysinternals via choco"
-                & choco install sysinternals -y
-
-                "Cloning the repository"
-
-                $destination = "E:\$using:repoName"
-                git clone $using:repoUrl $destination --quiet
-
-                Stop-Transcript
-            }
-            TestScript = {
-                return (Test-Path "E:\$using:repoName")
-            }
-            GetScript = {
-                # Do nothing
-            }
         }
     }
 }
---
+++ configmgr-lab-vmhost/scripts/configureHost.ps1
@@ -0,0 +1,141 @@
+# Logging
+$logFile = "$env:windir\temp\configureHost.log"
+
+function Write-HostLog {
+    param ($Text)
+    "[$(Get-Date -format "MM/dd/yyyy HH:mm:ss")] $Text" | Out-File -Append $logFile
+}
+
+Write-HostLog "START"
+
+# Change CD-ROM Drive Letter
+Write-HostLog "Changing CD-ROM DriveLetter to Z:"
+$x = Get-WmiObject -Class Win32_volume -Filter 'DriveType=5 AND DriveLetter="Z:"'
+if (-not $x) {
+    Get-WmiObject -Class Win32_volume -Filter 'DriveType=5' | Select-Object -First 1 | Set-WmiInstance -Arguments @{DriveLetter = 'Z:' }
+    $x = Get-WmiObject -Class Win32_volume -Filter 'DriveType=5 AND DriveLetter="Z:"'
+    if ($x) {
+        Write-HostLog "Changed Driveletter of CD-ROM drive"
+    }
+    else {
+        Write-HostLog "Failed to change Driveletter of CD-ROM drive. Exiting."
+        return
+    }
+}
+else {
+    Write-HostLog "CD-ROM already changed to Z:"
+}
+
+# Create Storage Pool
+$poolName = "StoragePool1"
+Write-HostLog "Creating Storage Pool named $poolName"
+$pool = Get-StoragePool -ErrorAction SilentlyContinue -FriendlyName $poolName
+if (-not $pool) {
+    New-StoragePool -FriendlyName $poolName -StorageSubSystemFriendlyName '*storage*' -PhysicalDisks (Get-PhysicalDisk -CanPool $True)
+    $pool = Get-StoragePool -ErrorAction SilentlyContinue -FriendlyName $poolName
+    if ($pool.OperationalStatus -eq 'OK') {
+        Write-HostLog "Storage Pool created."
+    }
+    else {
+        Write-HostLog "Storage Pool was not created. Exiting."
+        return
+    }
+}
+else {
+    Write-HostLog "Storage Pool already exists."
+}
+
+
+# Create Virtual Disks
+$virtualdiskName = "VirtualDisk1"
+Write-HostLog "Creating Virtual Disk named $virtualDiskName"
+$vdisk = Get-VirtualDisk -ErrorAction SilentlyContinue -friendlyName $virtualdiskName
+if (-not $vdisk) {
+    $disks = Get-StoragePool -FriendlyName $poolName -IsPrimordial $False | Get-PhysicalDisk
+    $diskNum = $disks.Count
+    New-VirtualDisk -StoragePoolFriendlyName $poolName -FriendlyName $virtualdiskName -ResiliencySettingName simple -NumberOfColumns $diskNum -UseMaximumSize
+    $vdisk = Get-VirtualDisk -ErrorAction SilentlyContinue -friendlyName $virtualdiskName
+    if ($vdisk.OperationalStatus -eq 'OK') {
+        Write-HostLog "Virtual Disk created with $diskNum disks."
+    }
+    else {
+        Write-HostLog "Virtual Disk was not created. Exiting."
+        return
+    }
+}
+else {
+    Write-HostLog "Virtual Disk already exists."
+}
+
+# Format virtual disks
+Write-HostLog "Formatting Virtual Disk $virtualDiskName"
+$vol = Get-Volume -ErrorAction SilentlyContinue -filesystemlabel $virtualdiskName
+if (-not $vol) {
+    Get-VirtualDisk -FriendlyName $virtualDiskName | Get-Disk | Initialize-Disk -Passthru | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -NewFileSystemLabel $virtualDiskName -AllocationUnitSize 64KB -FileSystem NTFS
+    $vol = Get-Volume -ErrorAction SilentlyContinue -filesystemlabel $virtualdiskName
+    if ($vol.filesystem -EQ 'NTFS') {
+        Write-HostLog "$virtualDiskName disk volume created."
+    }
+    else {
+        Write-HostLog "$virtualDiskName disk volume was not created. Exiting."
+        return
+    }
+}
+else {
+    Write-HostLog "$virtualDiskName disk volume already exists"
+}
+
+# Install choco
+Write-HostLog "Installing chocolatey"
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+# Install git
+Write-HostLog "Installing git via choco"
+& choco install git -y
+
+# Install sysinternals
+Write-HostLog "Installing sysinternals via choco"
+& choco install sysinternals -y
+
+# Reload PATH
+Write-HostLog "Reloading PATH"
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
+
+# Clone the repo
+Write-HostLog "Cloning the repository"
+$repoName = "memlabs"
+$repoUrl = "https://github.com/vinaypamnani-msft/$repoName"
+$destination = "$($vol.DriveLetter):\$repoName"
+if (-not (Test-Path $destination)) {
+    git clone $repoUrl $destination --quiet
+}
+else {
+    Write-HostLog "$repoName already cloned to $destination. Run git pull instead of trying to clone again."
+}
+
+# Install RRAS
+Write-HostLog "Installing RRAS"
+Install-WindowsFeature 'Routing', 'DirectAccess-VPN' -Confirm:$false -IncludeAllSubFeature -IncludeManagementTools
+
+# Configure NAT
+Write-HostLog "Configuring NAT"
+Install-RemoteAccess -VpnType RoutingOnly
+cmd.exe /c netsh routing ip nat install
+
+# External Hyper-V Switch name (created by Host DSC)
+$externalInterface = "vEthernet (External)"
+
+Write-HostLog "Adding $externalInterface interface to NAT"
+$text = & netsh routing ip nat show interface
+if ($text -like "*$externalInterface*") {
+    Write-HostLog "'$externalInterface' interface is already present in NAT."
+}
+else {
+    cmd.exe /c netsh routing ip nat add interface "$externalInterface"
+    cmd.exe /c netsh routing ip nat set interface "$externalInterface" mode=full
+}
+
+# Disable Modern Stack to allow use of RRAS GUI again
+cmd.exe /c reg add HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\RemoteAccess\Parameters /v ModernStackEnabled /t REG_DWORD /d 0 /f
+
+Write-HostLog "FINISH"
