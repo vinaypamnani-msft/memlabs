ad4b3c2d0255ff7c5478318d8e2fe7e3a74a8e2f
Vinay Pamnani <37223378+vinaypamnani-msft@users.noreply.github.com>
2021-09-16 00:30:18 -0400

    Delete vmbuild/zInput directory

--- vmbuild/zInput/filesToInject/staging/unattendOOBE/RunSysprepAAD.bat
+++
@@ -1,4 +0,0 @@
-REM This could be useful if you want to go through OOBE experience and AAD join a device.
-REM Unattend file here has the CopyProfile switch to maintain user customization across profiles.
-REM Run this batch file, start the VM and go through OOBE
-C:\Windows\system32\sysprep\sysprep.exe /generalize /oobe /shutdown /unattend:"C:\staging\UnattendAAD.xml"
