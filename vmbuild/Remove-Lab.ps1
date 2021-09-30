16e072f1c9b50c6f32d54ab435410e73475c7611
vinaypamnani-msft <vinpa@microsoft.com>
2021-09-29 22:05:23 -0400

    Add remove-lab

---
+++ vmbuild/Remove-Lab.ps1
@@ -0,0 +1,56 @@
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]
+    $DomainName
+)
+
+# Tell common to re-init
+if ($Common.Initialized) {
+    $Common.Initialized = $false
+}
+
+# Dot source common
+. $PSScriptRoot\Common.ps1
+
+# Set Verbose
+if ($PSCmdlet.MyInvocation.BoundParameters["Verbose"].IsPresent) {
+    $Common.VerboseEnabled = $true
+}
+else {
+    $Common.VerboseEnabled = $false
+}
+
+if ($DomainName) {
+    $vmsToDelete = Get-VMList -DomainName $DomainName
+    $scopesToDelete = Get-SubnetList -DomainName $DomainName
+}
+else {
+    $vmsToDelete = Get-VMList
+    $scopesToDelete = Get-SubnetList -DomainName $DomainName
+}
+
+foreach ($vm in $vmsToDelete) {
+    $VmName = $vm.VMName
+    $vmTest = Get-VM -Name $VmName -ErrorAction SilentlyContinue
+    if ($vmTest) {
+        Write-Log "Remove-Lab: $VmName`: Virtual machine exists.Removing."
+        if ($vmTest.State -ne "Off") {
+            Write-Log "Remove-Lab: $VmName`: Turning the VM off forcefully..."
+            $vmTest | Stop-VM -TurnOff -Force
+        }
+        $vmTest | Remove-VM -Force
+        Write-Log "Remove-Lab: $VmName`: Purging $($vmTest.Path) folder..."
+        Remove-Item -Path $($vmTest.Path) -Force -Recurse
+        Write-Log "Remove-Lab: $VmName`: Purge complete."
+    }
+}
+
+foreach ($scope in $scopesToDelete) {
+    $scopeId = $scope.Subnet
+    $dhcpScope = Get-DhcpServerv4Scope -ScopeID $scopeId -ErrorAction SilentlyContinue
+    if ($dhcpScope) {
+        Write-Log "Remove-Lab: $scopeId`: Scope exists.Removing."
+        $dhcpScope | Remove-DhcpServerv4Scope -Force -ErrorAction SilentlyContinue
+    }
+}
