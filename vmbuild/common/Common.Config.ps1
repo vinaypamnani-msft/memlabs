65e45de61e63897ee5d0ccb25cd871b16eb1f123
vinaypamnani-msft <vinpa@microsoft.com>
2021-11-10 17:58:06 -0500

    Fix validation

--- vmbuild/common/Common.Config.ps1
+++ vmbuild/common/Common.Config.ps1
@@ -258,7 +258,7 @@ function Test-ValidVmOptions {
 
         $existingSubnet = Get-List -Type Subnet | Where-Object { $_.Subnet -eq $($ConfigObject.vmoptions.network) | Select-Object -First 1 }
         if ($existingSubnet) {
-            if ($($ConfigObject.vmoptions.domainName) -ne $($existingSubnet.Domain)) {
+            if ($($ConfigObject.vmoptions.domainName.Trim()) -ne $($existingSubnet.Domain.Trim())) {
                 Add-ValidationMessage -Message "VM Options Validation: vmOptions.network [$($ConfigObject.vmoptions.network)] is in use by Domain [$($existingSubnet.Domain)]. You must specify a different network" -ReturnObject $ReturnObject -Warning
             }
             $CASorPRI = ($ConfigObject.virtualMachines.role -contains "CAS") -or (($ConfigObject.virtualMachines.role -contains "Primary"))
