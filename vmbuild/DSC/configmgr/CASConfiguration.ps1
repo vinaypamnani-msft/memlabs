f3bb3c1527598614996f6d6a0ec11b28c84ccb68
vinaypamnani-msft <vinpa@microsoft.com>
2021-11-18 13:18:50 -0500

    Move DotNet after domain join

--- vmbuild/DSC/configmgr/CASConfiguration.ps1
+++ vmbuild/DSC/configmgr/CASConfiguration.ps1
@@ -108,19 +108,8 @@
             VM        = $ThisVM | ConvertTo-Json
         }
 
-        WriteStatus InstallDotNet {
-            DependsOn = "[InitializeDisks]InitDisks"
-            Status    = "Installing .NET 4.7.2"
-        }
-
-        InstallDotNet472 DotNet {
-            DownloadUrl = "https://download.visualstudio.microsoft.com/download/pr/1f5af042-d0e4-4002-9c59-9ba66bcf15f6/089f837de42708daacaae7c04b7494db/ndp472-kb4054530-x86-x64-allos-enu.exe"
-            Ensure      = "Present"
-            DependsOn   = "[WriteStatus]InstallDotNet"
-        }
-
         SetCustomPagingFile PagingSettings {
-            DependsOn   = "[InstallDotNet472]DotNet"
+            DependsOn   = "[InitializeDisks]InitDisks"
             Drive       = 'C:'
             InitialSize = '8192'
             MaximumSize = '8192'
@@ -176,11 +165,22 @@
             Role      = "Site Server"
         }
 
+        WriteStatus InstallDotNet {
+            DependsOn = '[OpenFirewallPortForSCCM]OpenFirewall'
+            Status    = "Installing .NET 4.7.2"
+        }
+
+        InstallDotNet472 DotNet {
+            DownloadUrl = "https://download.visualstudio.microsoft.com/download/pr/1f5af042-d0e4-4002-9c59-9ba66bcf15f6/089f837de42708daacaae7c04b7494db/ndp472-kb4054530-x86-x64-allos-enu.exe"
+            Ensure      = "Present"
+            DependsOn   = "[WriteStatus]InstallDotNet"
+        }
+
         File ShareFolder {
             DestinationPath = $LogPath
             Type            = 'Directory'
             Ensure          = 'Present'
-            DependsOn       = '[OpenFirewallPortForSCCM]OpenFirewall'
+            DependsOn       = '[InstallDotNet472]DotNet'
         }
 
         FileReadAccessShare DomainSMBShare {
@@ -223,7 +223,7 @@
                     DownloadUrl = $sqlCUURL
                     FilePath    = $sqlCuDownloadPath
                     Ensure      = "Present"
-                    DependsOn = "[WriteStatus]DownloadSQLCU"
+                    DependsOn   = "[WriteStatus]DownloadSQLCU"
 
                 }
 
@@ -326,7 +326,7 @@
                 ReadNode      = "MachineJoinDomain"
                 ReadNodeValue = "Passed"
                 Ensure        = "Present"
-                DependsOn = "[DownloadSCCM]DownLoadSCCM"
+                DependsOn     = "[DownloadSCCM]DownLoadSCCM"
             }
 
             AddUserToLocalAdminGroup AddUserToLocalAdminGroup {
--- vmbuild/DSC/configmgr/DCConfiguration.ps1
+++ vmbuild/DSC/configmgr/DCConfiguration.ps1
@@ -89,19 +89,8 @@
             VM        = $ThisVM | ConvertTo-Json
         }
 
-        WriteStatus InstallDotNet {
-            DependsOn = "[InitializeDisks]InitDisks"
-            Status    = "Installing .NET 4.7.2"
-        }
-
-        InstallDotNet472 DotNet {
-            DownloadUrl = "https://download.visualstudio.microsoft.com/download/pr/1f5af042-d0e4-4002-9c59-9ba66bcf15f6/089f837de42708daacaae7c04b7494db/ndp472-kb4054530-x86-x64-allos-enu.exe"
-            Ensure      = "Present"
-            DependsOn   = "[WriteStatus]InstallDotNet"
-        }
-
         SetCustomPagingFile PagingSettings {
-            DependsOn   = "[InstallDotNet472]DotNet"
+            DependsOn   = "[InitializeDisks]InitDisks"
             Drive       = 'C:'
             InitialSize = '8192'
             MaximumSize = '8192'
@@ -236,11 +225,22 @@
             HashAlgorithm = "SHA256"
         }
 
+        WriteStatus InstallDotNet {
+            DependsOn = "[InstallCA]InstallCA"
+            Status    = "Installing .NET 4.7.2"
+        }
+
+        InstallDotNet472 DotNet {
+            DownloadUrl = "https://download.visualstudio.microsoft.com/download/pr/1f5af042-d0e4-4002-9c59-9ba66bcf15f6/089f837de42708daacaae7c04b7494db/ndp472-kb4054530-x86-x64-allos-enu.exe"
+            Ensure      = "Present"
+            DependsOn   = "[WriteStatus]InstallDotNet"
+        }
+
         File ShareFolder {
             DestinationPath = $LogPath
             Type            = 'Directory'
             Ensure          = 'Present'
-            DependsOn       = "[InstallCA]InstallCA"
+            DependsOn       = "[InstallDotNet472]DotNet"
         }
 
         FileReadAccessShare DomainSMBShare {
