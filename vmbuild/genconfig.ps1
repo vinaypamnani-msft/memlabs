ff653ec97d17d80c01fed06026e549b4fd9f6013
timhe <timhe@microsoft.com>
2022-01-18 13:02:03 -0500

    OS selection is smarter

--- vmbuild/genconfig.ps1
+++ vmbuild/genconfig.ps1
@@ -2168,6 +2168,38 @@ function get-ValidResponse {
     return $response
 }
 
+Function Get-SupportedOperatingSystemsForRole {
+    param (
+        [Parameter(Mandatory = $true, HelpMessage = "role")]
+        [string] $role
+    )
+
+    $ServerList = $Common.Supported.OperatingSystems | Where-Object { $_ -like 'Server*' }
+    $ClientList = $Common.Supported.OperatingSystems | Where-Object { $_ -notlike 'Server*' }
+    $AllList = $Common.Supported.OperatingSystems
+    switch ($role) {
+        "DC" { return $ServerList }
+        "CAS" { return $ServerList }
+        "CAS and Primary" { return $ServerList }
+        "Primary" { return $ServerList }
+        "Secondary" { return $ServerList }
+        "FileServer" { return $ServerList }
+        "DPMP" { return $ServerList }
+        "DomainMember" { return $AllList }
+        "DomainMember (Server)" { return $ServerList }
+        "DomainMember (Client)" { return $ClientList }
+        "WorkgroupMember" { return $AllList }
+        "InternetClient" { return $ClientList }
+        "AADClient" { return $ClientList }
+        "OSDClient" { return $null }
+        default {
+            return $AllList
+        }
+    }
+    return $AllList
+}
+
+
 Function Get-OperatingSystemMenu {
     [CmdletBinding()]
     param (
@@ -2181,7 +2213,11 @@ Function Get-OperatingSystemMenu {
 
     $valid = $false
     while ($valid -eq $false) {
-        $property."$name" = Get-Menu "Select OS Version" $($Common.Supported.OperatingSystems) $CurrentValue -Test:$false
+        $OSList = Get-SupportedOperatingSystemsForRole -role $property.role
+        if ($null -eq $OSList ){
+            return
+        }
+        $property."$name" = Get-Menu "Select OS Version" $OSList $CurrentValue -Test:$false
         if (Get-TestResult -SuccessOnWarning -NoNewLine) {
             return
         }
