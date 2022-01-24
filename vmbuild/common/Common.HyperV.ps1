e5bb989f45e71158feb8fd928f6f3b21f972fefa
vinaypamnani-msft <vinpa@microsoft.com>
2022-01-24 13:31:25 -0500

    Add Start-VM2 (again)

--- vmbuild/common/Common.HyperV.ps1
+++ vmbuild/common/Common.HyperV.ps1
@@ -13,3 +13,36 @@ function Get-VM2 {
         return [System.Management.Automation.Internal.AutomationNull]::Value
     }
 }
+
+function Start-VM2 {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$Name,
+        [Parameter(Mandatory = $false)]
+        [switch]$Passthru
+    )
+
+    $vm = Get-VM2 -Name $Name
+
+    if ($vm) {
+        try {
+            Start-VM -VM $vm -ErrorAction Stop
+            if ($Passthru.IsPresent) {
+                return $true
+            }
+        }
+        catch {
+            Write-Log "$Name`: Failed to start VM. $($_.Exception.Message)" -Failure
+            if ($Passthru.IsPresent) {
+                return $false
+            }
+        }
+    }
+    else {
+        if ($Passthru.IsPresent) {
+            Write-Log "$Name`: VM was not found in Hyper-V." -Warning
+            return $false
+        }
+    }
+}
