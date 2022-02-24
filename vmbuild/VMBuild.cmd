2784b83326ec83c7b97e99ab6f27c873e4688f93
vinaypamnani-msft <vinpa@microsoft.com>
2022-02-24 11:00:53 -0500

    Use PS7

--- vmbuild/VMBuild.cmd
+++ vmbuild/VMBuild.cmd
@@ -1,6 +1,24 @@
 @ECHO OFF
 pushd "%~dp0"
+
 @ECHO ON
 git pull
+
+@ECHO OFF
+IF NOT EXIST "C:\Program Files\PowerShell\7\pwsh.exe" GOTO INSTALLPS7
+IF EXIST "C:\Program Files\PowerShell\7\pwsh.exe" GOTO PS7
+
+:INSTALLPS7
+choco install pwsh -y
+IF EXIST "C:\Program Files\PowerShell\7\pwsh.exe" GOTO PS7 ELSE GOTO PS5
+
+:PS7
+"C:\Program Files\PowerShell\7\pwsh.exe" -ExecutionPolicy Bypass -NoLogo -NoExit -Command "./New-Lab.ps1"
+GOTO END
+
+:PS5
 powershell -ExecutionPolicy Bypass -NoLogo -NoExit -Command "./New-Lab.ps1"
+GOTO END
+
+:END
 popd
