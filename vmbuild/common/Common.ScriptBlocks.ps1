95e4f22ec2a9d23dc26f02f7de075a91320161bc
timhe <timhe>
2024-04-25 13:07:42 -0400

    Fix useFakeWsusServer - fs removal

--- vmbuild/common/Common.ScriptBlocks.ps1
+++ vmbuild/common/Common.ScriptBlocks.ps1
@@ -1730,7 +1730,7 @@ $global:VM_Config = {
             }
 
             $result = Invoke-VmCommand -VmName $currentItem.vmName -VmDomainName $domainName -ScriptBlock $disable_AutomaticUpdates -DisplayName "Disable Automatic Updates"
-fs
+
             $disable_AutomaticUpdatesFakeWSUS = {
                 New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Force
                 New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" -Name "WUServer" -Type String -Value "http://localhost" -Force
