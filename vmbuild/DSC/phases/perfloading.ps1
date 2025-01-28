51808ae79e474b65cd04345401e649ab0c7af2d3
timhe <timhe@microsoft.com>
2025-01-28 09:41:49 -0500

    Remove extra code from perfloading

--- vmbuild/DSC/phases/perfloading.ps1
+++ vmbuild/DSC/phases/perfloading.ps1
@@ -1,34 +1,3 @@
-#
-# Press 'F5' to run this script. Running this script will load the ConfigurationManager
-# module for Windows PowerShell and will connect to the site.
-#
-# This script was auto-generated at '1/22/2025 9:52:16 AM'.
-
-# Site configuration
-$SiteCode = "CS1" # Site code 
-$ProviderMachineName = "CT1-CS1SITE.cstest1.com" # SMS Provider machine name
-
-# Customizations
-$initParams = @{}
-#$initParams.Add("Verbose", $true) # Uncomment this line to enable verbose logging
-#$initParams.Add("ErrorAction", "Stop") # Uncomment this line to stop the script on any errors
-
-# Do not change anything below this line
-
-# Import the ConfigurationManager.psd1 module 
-if((Get-Module ConfigurationManager) -eq $null) {
-    Import-Module "$($ENV:SMS_ADMIN_UI_PATH)\..\ConfigurationManager.psd1" @initParams 
-}
-
-# Connect to the site's drive if it is not already present
-if((Get-PSDrive -Name $SiteCode -PSProvider CMSite -ErrorAction SilentlyContinue) -eq $null) {
-    New-PSDrive -Name $SiteCode -PSProvider CMSite -Root $ProviderMachineName @initParams
-}
-
-# Set the current location to be the site code.
-Set-Location "$($SiteCode):\" @initParams
-
-
 #perfloading.ps1
 param(
     [string]$ConfigFilePath,
