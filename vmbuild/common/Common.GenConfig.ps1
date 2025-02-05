19447f9ffb5b4956b10f29277c333c7f4355b6eb
timhe <timhe@microsoft.com>
2025-02-05 15:10:40 -0500

    Fix crash with wsusDataBaseServer

--- vmbuild/common/Common.GenConfig.ps1
+++ vmbuild/common/Common.GenConfig.ps1
@@ -965,8 +965,8 @@ function ConvertTo-DeployConfigEx {
                         }
                     }
                 }
-                if ($thisVM.wsusDataBaseServer -and $thisVM.wsusDataBaseServer -ne "WID")  {
-                    $sqlVM = get-list -type VM -domain $DomainName | Where-Object { $_.VmName -eq $thisVM.wsusDataBaseServer  }
+                if ($thisVM.wsusDataBaseServer -and $thisVM.wsusDataBaseServer -ne "WID")  {                    
+                    $sqlVM = $deployConfig.virtualMachines | Where-Object { $_.VmName -eq $thisVM.wsusDataBaseServer  }
                     
                     Add-VMToAccountLists -thisVM $thisVM -VM $sqlVM -accountLists $accountLists -deployConfig $deployconfig -LocalAdminAccounts -SQLSysAdminAccounts -WaitOnDomainJoin
                 }
