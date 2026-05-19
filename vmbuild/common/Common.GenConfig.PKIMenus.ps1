5434c33727c6e4b02bcf619405ea412b6cbd0e49
Tim Helton <timhe@microsoft.com>
2026-05-19 16:36:47 -0400

    PKI menu: use uppercase ISSUINGCA to match other VM name convention

--- vmbuild/common/Common.GenConfig.PKIMenus.ps1
+++ vmbuild/common/Common.GenConfig.PKIMenus.ps1
@@ -89,7 +89,7 @@ function Select-IssuingCAVMMenu {
     }
     switch ($result.ToLowerInvariant()) {
         "n" {
-            $result = Add-NewVMForRole -Role "DomainMember" -Domain $ConfigToModify.vmOptions.DomainName -ConfigToModify $ConfigToModify -Name "IssuingCA" -ReturnMachineName:$true
+            $result = Add-NewVMForRole -Role "DomainMember" -Domain $ConfigToModify.vmOptions.DomainName -ConfigToModify $ConfigToModify -Name "ISSUINGCA" -ReturnMachineName:$true
         }
     }
     return $result
