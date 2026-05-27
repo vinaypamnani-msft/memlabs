f8353aa564d16c74138c2f08144f71701f74602c
Tim Helton <timhe@microsoft.com>
2026-05-26 23:23:22 -0400

    Add all required mRemoteNG attributes to connection nodes
    mRemoteNG throws NullReferenceException when loading entries that are
    missing expected attributes. Our nodes only had ~15 attributes; a
    valid mRemoteNG entry has ~70+.
    
    - Add all RDP display/redirect/gateway attributes with defaults
      (Resolution=FitToWindow, Colors=Colors16Bit, AutomaticResize=true,
      RDPAuthenticationLevel=NoAuth, etc.)
    - Add all VNC attributes with defaults
    - Add all 50+ Inherit* attributes explicitly set to 'false'
    - Compress JSON in Description field (-Depth 4 -Compress) to eliminate
      encoded CRLF characters in the XML attribute
    
    Attribute list and defaults taken from a known-working mRemoteNG
    confCons.xml with ConfVersion=2.6.

--- vmbuild/common/Common.mRemoteNG.ps1
+++ vmbuild/common/Common.mRemoteNG.ps1
@@ -223,16 +223,78 @@ function New-MRemoteNGConnectionNode {
     $node.SetAttribute("Password", $Password)
     $node.SetAttribute("Hostname", $Hostname)
     $node.SetAttribute("Protocol", $Protocol)
-    $node.SetAttribute("Port", $Port)
     $node.SetAttribute("PuttySession", $PuttySession)
+    $node.SetAttribute("Port", $Port)
     $node.SetAttribute("ConnectToConsole", "false")
     $node.SetAttribute("UseCredSsp", $(if ($Protocol -eq "RDP") { "true" } else { "false" }))
+    $node.SetAttribute("RenderingEngine", "IE")
+    $node.SetAttribute("ICAEncryptionStrength", "EncrBasic")
+    $node.SetAttribute("RDPAuthenticationLevel", "NoAuth")
+    $node.SetAttribute("RDPMinutesToIdleTimeout", "0")
+    $node.SetAttribute("RDPAlertIdleTimeout", "false")
+    $node.SetAttribute("LoadBalanceInfo", "")
+    $node.SetAttribute("Colors", "Colors16Bit")
+    $node.SetAttribute("Resolution", "FitToWindow")
+    $node.SetAttribute("AutomaticResize", "true")
+    $node.SetAttribute("DisplayWallpaper", "false")
+    $node.SetAttribute("DisplayThemes", "false")
+    $node.SetAttribute("EnableFontSmoothing", "false")
+    $node.SetAttribute("EnableDesktopComposition", "false")
+    $node.SetAttribute("CacheBitmaps", "false")
+    $node.SetAttribute("RedirectDiskDrives", "false")
+    $node.SetAttribute("RedirectPorts", "false")
+    $node.SetAttribute("RedirectPrinters", "false")
+    $node.SetAttribute("RedirectSmartCards", "false")
+    $node.SetAttribute("RedirectSound", "DoNotPlay")
+    $node.SetAttribute("SoundQuality", "Dynamic")
+    $node.SetAttribute("RedirectKeys", "false")
     $node.SetAttribute("Connected", "false")
+    $node.SetAttribute("PreExtApp", "")
+    $node.SetAttribute("PostExtApp", "")
+    $node.SetAttribute("MacAddress", "")
+    $node.SetAttribute("UserField", "")
+    $node.SetAttribute("ExtApp", "")
+    $node.SetAttribute("VNCCompression", "CompNone")
+    $node.SetAttribute("VNCEncoding", "EncHextile")
+    $node.SetAttribute("VNCAuthMode", "AuthVNC")
+    $node.SetAttribute("VNCProxyType", "ProxyNone")
+    $node.SetAttribute("VNCProxyIP", "")
+    $node.SetAttribute("VNCProxyPort", "0")
+    $node.SetAttribute("VNCProxyUsername", "")
+    $node.SetAttribute("VNCProxyPassword", "")
+    $node.SetAttribute("VNCColors", "ColNormal")
+    $node.SetAttribute("VNCSmartSizeMode", "SmartSAspect")
+    $node.SetAttribute("VNCViewOnly", "false")
+    $node.SetAttribute("RDGatewayUsageMethod", "Never")
+    $node.SetAttribute("RDGatewayHostname", "")
+    $node.SetAttribute("RDGatewayUseConnectionCredentials", "Yes")
+    $node.SetAttribute("RDGatewayUsername", "")
+    $node.SetAttribute("RDGatewayPassword", "")
+    $node.SetAttribute("RDGatewayDomain", "")
 
     if (-not [string]::IsNullOrWhiteSpace($VmId)) {
         $node.SetAttribute("VmId", $VmId)
         $node.SetAttribute("UseVmId", "true")
         $node.SetAttribute("UseEnhancedMode", $UseEnhancedMode.ToString().ToLower())
+    }
+
+    # All Inherit* attributes must be present or mRemoteNG throws NullReferenceException
+    $inheritAttrs = @(
+        "CacheBitmaps", "Colors", "Description", "DisplayThemes", "DisplayWallpaper",
+        "EnableFontSmoothing", "EnableDesktopComposition", "Domain", "Icon", "Panel",
+        "Password", "Port", "Protocol", "PuttySession", "RedirectDiskDrives", "RedirectKeys",
+        "RedirectPorts", "RedirectPrinters", "RedirectSmartCards", "RedirectSound",
+        "SoundQuality", "Resolution", "AutomaticResize", "UseConsoleSession", "UseCredSsp",
+        "RenderingEngine", "Username", "ICAEncryptionStrength", "RDPAuthenticationLevel",
+        "RDPMinutesToIdleTimeout", "RDPAlertIdleTimeout", "LoadBalanceInfo", "PreExtApp",
+        "PostExtApp", "MacAddress", "UserField", "ExtApp", "VNCCompression", "VNCEncoding",
+        "VNCAuthMode", "VNCProxyType", "VNCProxyIP", "VNCProxyPort", "VNCProxyUsername",
+        "VNCProxyPassword", "VNCColors", "VNCSmartSizeMode", "VNCViewOnly",
+        "RDGatewayUsageMethod", "RDGatewayHostname", "RDGatewayUseConnectionCredentials",
+        "RDGatewayUsername", "RDGatewayPassword", "RDGatewayDomain"
+    )
+    foreach ($attr in $inheritAttrs) {
+        $node.SetAttribute("Inherit$attr", "false")
     }
 
     return $node
@@ -569,7 +631,7 @@ function New-MRemoteNGFileFromHyperV {
                 }
             }
 
-            $comment = $c | ConvertTo-Json
+            $comment = $c | ConvertTo-Json -Depth 4 -Compress
             $name = $vm.VmName
             $ForceOverwrite = $true
             $vmID = $null
@@ -702,7 +764,7 @@ function New-MRemoteNGFileFromHyperV {
             foreach ($item in $vm | Get-Member -MemberType NoteProperty | Where-Object { $null -ne $vm."$($_.Name)" }) {
                 $c | Add-Member -MemberType NoteProperty -Name $item.Name -Value $vm."$($item.Name)" -Force
             }
-            $comment = $c | ConvertTo-Json
+            $comment = $c | ConvertTo-Json -Depth 4 -Compress
             $displayName = $vm.VmName
 
             $protocol = "RDP"
