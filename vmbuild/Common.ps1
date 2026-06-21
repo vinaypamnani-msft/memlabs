62e83213afca78db01bced87c71a8bd7467899b9
timhe <timhe@microsoft.com>
2026-06-21 09:23:04 -0400

    Update Mega config

--- vmbuild/Common.ps1
+++ vmbuild/Common.ps1
@@ -8381,8 +8381,8 @@ if (-not $Common.Initialized) {
         }
 
         $global:Common = [PSCustomObject]@{
-            MemLabsVersion              = "260620.2"
-            LatestHotfixVersion         = "260620.2"
+            MemLabsVersion              = "260621.0"
+            LatestHotfixVersion         = "260621.0"
             PS7                         = $PS7
             Initialized                 = $true
             InJob                       = $InJob
