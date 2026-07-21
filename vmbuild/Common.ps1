1edac15e8c6a511b931d15a161b06005a5bc3fe2
timhe <timhe@microsoft.com>
2026-07-21 12:19:18 -0400

    Updade dsc.zip

--- vmbuild/Common.ps1
+++ vmbuild/Common.ps1
@@ -9417,8 +9417,8 @@ if (-not $Common.Initialized) {
         }
 
         $global:Common = [PSCustomObject]@{
-            MemLabsVersion              = "260721.0"
-            LatestHotfixVersion         = "260721.0"
+            MemLabsVersion              = "260721.1"
+            LatestHotfixVersion         = "260721.1"
             PS7                         = $PS7
             Initialized                 = $true
             InJob                       = $InJob
